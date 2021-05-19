813840414b5facd68e9ff8925bb45f96b4ee9043
karityan <karin141227@gmail.com>
2021-05-19 16:48:53 +0900

    詳細ページ修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,13 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
   end
 
   def new
-    @prototype = Prototype.new
+  @prototype = Prototype.new
   end
 
   def create
@@ -49,10 +49,8 @@ class PrototypesController < ApplicationController
   end
 
   private
-
+  
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
-
 end
