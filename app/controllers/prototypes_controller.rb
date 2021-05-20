bd480497ba59d27be2ff964c0e0496e2cfb599c7
karityan <karin141227@gmail.com>
2021-05-20 11:36:16 +0900

    詳細、削除ページへ移行　ログアウト状態の詳細閲覧

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -49,8 +48,9 @@ class PrototypesController < ApplicationController
   end
 
   private
-  
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
 end
